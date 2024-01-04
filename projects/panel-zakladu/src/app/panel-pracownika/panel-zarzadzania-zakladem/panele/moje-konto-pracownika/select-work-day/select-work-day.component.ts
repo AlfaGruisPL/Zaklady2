f22af4e16da74461f1a39ed4ef72f7e5f4a0327f
AlfaGruisPL <alfagruis@gmail.com>
2024-01-04 09:16:57 +0100

    work

---
+++ projects/panel-zakladu/src/app/panel-pracownika/panel-zarzadzania-zakladem/panele/moje-konto-pracownika/select-work-day/select-work-day.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-select-work-day',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './select-work-day.component.html',
+  styleUrl: './select-work-day.component.scss',
+})
+export class SelectWorkDayComponent {
+  @Input({ required: true }) WorkDay!: {
+    monday: boolean;
+    tuesday: boolean;
+    wednesday: boolean;
+    thursday: boolean;
+    friday: boolean;
+    saturday: boolean;
+    sunday: boolean;
+  };
+  @Output() WorkDayChange = new EventEmitter();
+
+  change(day: string) {
+    // @ts-ignore
+    this.WorkDay[day] = !this.WorkDay[day];
+    this.WorkDayChange.emit(this.WorkDay);
+  }
+}
