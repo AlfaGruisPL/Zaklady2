0247cdc2a4ddc24bc2fd69e43e3e01d24b11891d
AlfaGruisPL <alfagruis@gmail.com>
2024-01-17 16:25:25 +0100

    work
    Took 1 hour 0 minutes

--- projects/panel-zakladu/src/app/klasy/panelPracownika/stronaReklamowa/ustawienia-strony-reklamowej.ts
+++ projects/panel-zakladu/src/app/klasy/panelPracownika/stronaReklamowa/ustawienia-strony-reklamowej.ts
@@ -2,9 +2,10 @@ export class UstawieniaStronyReklamowejDto {
   stronaReklamowaPrzelacznik: boolean | undefined = false;
   data: FirstTemplate | {} = {};
   template: string | undefined = '';
+  keywords: string | undefined = '';
+  miniDescription: string | undefined = '';
 
   constructor(zrodlo: Partial<UstawieniaStronyReklamowej> = {}) {
-    console.log(zrodlo);
     switch (zrodlo.template) {
       case 'template1':
         Object.assign(this.data, zrodlo.FirstTemplateData);
@@ -15,6 +16,9 @@ export class UstawieniaStronyReklamowejDto {
     }
     this.stronaReklamowaPrzelacznik = zrodlo.stronaReklamowaPrzelacznik;
     this.template = zrodlo.template;
+    this.keywords = zrodlo.keywords;
+
+    this.miniDescription = zrodlo.miniDescription;
   }
 }
 
@@ -23,6 +27,9 @@ export class UstawieniaStronyReklamowej {
   stronaReklamowaPrzelacznik: boolean | undefined = false;
   FirstTemplateData = new FirstTemplate();
   SecondTemplateData = new SecondTemplateTmp();
+
+  miniDescription = '';
+  keywords: string = '';
 
   wstawDane(dane: Partial<UstawieniaStronyReklamowej> | any) {
     this.stronaReklamowaPrzelacznik = dane['wlaczona'];
@@ -165,65 +172,3 @@ export class SecondTemplateTmp {
     };
   }
 }
-
-
-/*
-  header: {
-        // todo menu
-        nav: {
-          logo: "",
-          //   menu: {},
-        },
-        title: "Enjoy our barber services",
-        backgroundImage: "",
-        description:
-          "Establish your new amazing look with Barbershop services! From haircuts to hot shaves, you can experience the best level of barbering in the area.",
-      },
-      aboutUs: {
-        information: {
-          title: "Who We Are",
-          shortDescription:
-            "Barbershop is a team of highly qualified barbers dedicated to offering the best value. We do this by providing high quality salon services for men and boys, in a warm, friendly atmosphere.",
-          mainDescription:
-            "Welcome to our distinguished hair salon, where we merge the artistry of hairstyling with a commitment to providing a personalized and exceptional experience. Our team comprises skilled and passionate stylists who are not only experts in their craft but also dedicated to staying abreast of the latest trends and techniques.\n" +
-            "\n" +
-            "Upon entering our contemporary space, you'll find an ambiance that invites relaxation and creativity. Our consultations are thorough, ensuring we understand your unique preferences and lifestyle, enabling us to tailor our services to complement your individuality. From classic cuts to avant-garde styles, our diverse range of services caters to every taste.\n" +
-            "\n" +
-            "Education is at the core of our philosophy, and our stylists undergo continuous training to master the latest industry innovations. This commitment allows us to offer a spectrum of services, including precision cuts, creative color treatments, and transformative styling.",
-        },
-        images: {
-          mainImage: "",
-          otherImages: {
-            image1: "",
-            image2: "",
-            image3: "",
-          },
-        },
-      },
-      services: {
-        title: "OUR BARBER SERVICES",
-        subPage: {
-          title: "Our Service",
-        },
-      },
-      employees: {
-        backgroundImage: "",
-        title: "Our barbers",
-        shortDescription:
-          "We employ only highly qualified barbers who are not just professionals, but also enjoy maintaining the atmosphere of a classic barbershop.",
-      },
-      pricing: {
-        title: "Our barber pricing",
-        subPage: {
-          title: "Our Pricing",
-          shortDescription:
-            "We employ only highly qualified barbers who are not just professionals, but also enjoy maintaining the atmosphere of a classic barbershop.",
-        },
-      },
-      getInTouch: {
-        backgroundImage: "",
-        title: "Get in touch",
-        shortDescription:
-          "Leave your name and phone number, and we’ll contact you.",
-      },
- */
